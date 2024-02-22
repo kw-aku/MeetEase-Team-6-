42cdf59a8585bf486866ba87efbccd0c91adaca6
Justice Quagraine <justicequagrainejnr@gmail.com>
2024-02-22 10:39:27 +0000

    Index.php with css folder with css and Images

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php 
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset = UTF-8>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/meetease.css">
+    <title>Register</title>  
+
+
+</head>
+
+<body >
+<!--Left content-->
+<div style = "padding-right: 500px; padding-top: 80px; "> 
+    <div style= "padding: 100px; border: 10px solid black; border-left: 0px; border-radius: 50px">
+        <p style = "font-size: 60px;">Welcome to MeetEase <br>Where meeting is made convenient</p>
+        <div>
+            <p style= "font-size: 18px;">Make Meetings, Join Meetings and Never Miss Out on a Meeting</p>
+    
+        </div>
+        
+        <button onclick = "window.location.href= 'register.html'" style= "border: 0px; background-color: rgba(2, 0, 0, 0.643); color: white; border-radius: 10px; font-size: 18px; box-shadow: 1px 5px 10px rgba(131, 255, 7, 0.52); "> JOIN US TODAY <img src= "Images/next.png" style="width: 13px; height: 13px; " ></button>
+    </div>
+</div>  
+
+<!--image at the right-->
+<div style="position: absolute; top: 0; left: 1070px; padding-top: 150px;">
+    <img src="Images/Black White Simple People Bookmark.jpg" style="width: 350px; height:auto; border-radius: 5px; ">
+</div>
+
+    
+
+    
+
+
+</body>
+
+</html>
