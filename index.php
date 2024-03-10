6b5e38b0d448389057d8ab7c752dec4ac1e680d3
kw-aku <111888816+kw-aku@users.noreply.github.com>
2024-03-10 10:37:58 +0000

    Update index.php
    This file contains the html and css (internal) content for the index page

--- index.php
+++ index.php
@@ -1,43 +1,134 @@
-<?php 
-
-
-
-?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta charset = UTF-8>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/meetease.css">
-    <title>MeetEase</title>  
-
-
+    <link type="css/text" rel="stylesheet" href="in.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com"> <!---->
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> <!--for the font Inter-->
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet"> <!---->
+    <title>Document</title>
 </head>
-
 <body>
-<!--Left content-->
-<div style = "padding-right: 500px; padding-top: 80px; position: fixed;"> 
-    <div style= "padding: 100px; border: 10px solid black; border-left: 0px; border-radius: 50px">
-        <p style = "font-size: 60px;">Welcome to MeetEase <br>Where meeting is made convenient</p>
-        <div>
-            <p style= "font-size: 18px;">Make Meetings, Join Meetings and Never Miss Out on a Meeting</p>
-    
-        </div>
-        
-        <button onclick = "window.location.href= 'register.html'" style= "border: 0px; background-color: rgba(2, 0, 0, 0.643); color: white; border-radius: 50px; font-size: 18px; box-shadow: 1px 5px 10px rgba(131, 255, 7, 0.52); "> JOIN US TODAY <img src= "Images/next.png" style="width: 13px; height: 13px; " ></button>
+    <div id="nav-bar">
+        <p id="team-logo">[ ] MeetEase</p>
+        <a href="loginpage.html"><p id="login-btn">Login</p></a>
     </div>
-</div>  
 
-<!--image at the right-->
-<div style="position: fixed; top: 40px; left: 1090px; padding-top: 150px;  ">
-    <img src="Images/Black White Simple People Bookmark.jpg" style="width: 300px; height:auto;border: 2px solid black; border-radius: 50px;box-shadow: 0px 5px 10px black; border-left: 0px; ">
-</div>
+    <div id="txt-content">
+        <div id="hero-txt">
+            <p>Connect,</p>
+            <p>Share and</p>
+            <p>Engage</p>
+            <p class="txt-shade" id="first-shade"></p>
+            <p class="txt-shade" id="second-shade"></p>
+            <p class="txt-shade" id="third-shade"></p>
+        </div>
 
-    
+        <p id="sub-hero-txt">Join the Movement. MeetEase makes <br>
+            connecting with people a breeze</p>
 
-    
 
+        <a href="register.html"><p id="txt"> Start Now -> </p></a>
+        
+    </div>
 
+    <div id="img-content">
+        <img src="./assests/teams-teamwork-to-assemble-a-working-puzzle-transformed.png"
+             alt="people holding pieces of jigsaw puzzle"/>
+    </div>
 </body>
+
+<style>
+    body{
+        font-family: "Inter";
+    }
+
+    #nav-bar{
+        position: relative;
+        display: flex;
+        gap: 70em;
+        padding-left: 2em;
+    }
+
+    #team-logo{
+        font-weight: 700;
+    }
+
+    #login-btn{
+        transition: .3s;
+        cursor: pointer;
+    }
+
+    #login-btn:hover{
+        transform: scale(1.1);
+        font-weight: 700;
+    }
+
+    #txt-content{
+        float: left;
+    }
+
+    #hero-txt{
+        font-size: 60px;
+        margin-left: 2em;
+        margin-top: 2.3em;
+        font-weight: 700;
+        line-height: 1em;
+    }
+
+    #img-content{
+        float: right;
+    }
+
+    #first-shade,#second-shade,#third-shade{
+        position: absolute;
+        width: 4.5em;
+        height: .5em;
+        background-color: #D9D9D9;
+    }
+
+    #first-shade{
+        bottom: 8.3em;
+        z-index: -1;
+    }
+
+    #second-shade{
+        bottom: 6.4em;
+        z-index: -1;
+        width: 4.9em;
+    }
+
+    #third-shade{
+        bottom: 4.3em;
+        z-index: -1;
+        width: 3.8em;
+    }
+
+    #sub-hero-txt{
+        margin-left: 7em;
+        margin-top: 2.3em;
+        line-height: 1.6em;
+    }
+
+    #txt{
+        margin-left: 7em;
+        margin-top: 2.3em;
+        font-weight: 100;
+        transition: .3s;
+        cursor: pointer;
+    }
+
+    #txt:hover{
+        transform: scale(1.1);
+        font-weight: 700;
+    }
+
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
+
+</style>
 
 </html>
