16d300023a8f16907fd4efa00ff4dc1f398d77d3
Mwin-nom <123468471+Mwin-nom@users.noreply.github.com>
2024-03-16 21:20:06 +0000

    Creating login_action.php

---
+++ login_action.php
@@ -0,0 +1,66 @@
+<?php
+// Start the session
+session_start();
+include "connections.php";
+global $conn;
+
+
+if (isset($_POST['submit_button'])) {
+    //Collecting form data and storing in variables
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    
+    
+
+   
+
+
+//query to select record from people table using email
+    $emailQuery = "SELECT * FROM user WHERE email = '$email'";
+
+    
+
+// Execute the query
+    $emailResult = mysqli_query($conn, $emailQuery);
+    
+
+   
+
+
+//Checking if any rows were returned
+    if (mysqli_num_rows($emailResult) > 0) {
+        // At least one row was returned
+       //Fetching record
+        $fetchedRecord= mysqli_fetch_assoc($emailResult);
+        //Fetching password from record
+        $hashedPasswordFromDb = $fetchedRecord['password'];
+        $user_id= $fetchedRecord['id'];
+        
+    
+        
+
+        //Checking if password is the same
+        if (password_verify($password, $hashedPasswordFromDb)) {
+            
+            echo "Password verified. Login successful.";
+
+            $_SESSION['user_id'] = $user_id;
+            
+
+
+            header("Location: homepage.php");
+            exit();
+        } else {
+            // Passwords don't match
+            header("Location: loginpage.php");
+           
+            
+        
+        }
+    } else {
+        // No rows were returned
+        echo "No records found.";
+
+
+    }
+}
