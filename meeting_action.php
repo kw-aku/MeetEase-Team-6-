69f3b973e19e511816aa68d0e36ec43a9746b62a
kw-aku <111888816+kw-aku@users.noreply.github.com>
2024-03-17 14:02:05 +0000

    Update meeting_action.php

--- meeting_action.php
+++ meeting_action.php
@@ -6,9 +6,8 @@ global $conn;
 session_start();
 $user_id = $_SESSION['user_id'];
 
-
 //collecting user inputs
-if ($_SERVER["REQUEST_METHOD"] == "POST") { //checking if the submit button has been clicked
+if (isset($_POST['submit_meeting'])) { //checking if the submit button has been clicked
     //collecting user inputs from from in "user_homepage.html"
     $title = mysqli_real_escape_string($conn, $_POST['meeting_title']);
     $description = mysqli_real_escape_string($conn, $_POST['description']); 
@@ -16,15 +15,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") { //checking if the submit button has 
     $date = mysqli_real_escape_string($conn, $_POST['date']);
     $start_time = mysqli_real_escape_string($conn, $_POST['start_time']);
     $end_time = mysqli_real_escape_string($conn, $_POST['end_time']);
-      
-    //sql query
-    $sql = "INSERT INTO meeting (title, user_id, `desc`, date, stime, etime, venue) VALUES ( $title,$user_id, $description, $date, $start_time, $end_time, $venue);";
-    $result = mysqli_query($conn, $emailQuery);
     
+
+
+    //sql query
+    $sql = "INSERT INTO meeting (title, user_id, desc_meeting, datemade, stime, etime, venue) VALUES ('$title', '$user_id', '$description', '$date', '$start_time', '$end_time', '$venue')";
+    $result = $conn->query($sql);
+    
+
     //checking if execution executes
     if ($result) { 
         echo "<script>alert('Meeting Added');</script>";
-        header("Location: user_homepage.php");//redirecting to user_homepage.php
+        header("Location: homepage.php");//redirecting to user_homepage.php
     } else {
         echo "<script>alert('Add Meeting Failed');</script>";
         echo "<script>window.location.href = 'register.php';</script>";
